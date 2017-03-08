07e67fa6ee8ccd2f4f2c6b937ecf89df90ce919e
sseifert <sseifert@pro-vision.de>
2017-03-08 13:21:37 +0100

    start with copy from Maven NodeJS Proxy

---
+++ maven-aem-binaries-proxy/src/main/java/io/wcm/devops/maven/aembinariesproxy/MavenProxyConfiguration.java
@@ -0,0 +1,131 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2017 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.devops.maven.aembinariesproxy;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.client.HttpClientConfiguration;
+
+/**
+ * Configuration for Maven AEM Binaries Proxy.
+ */
+public class MavenProxyConfiguration extends Configuration {
+
+  @NotEmpty
+  private String groupId;
+  @NotEmpty
+  private String nodeJsArtifactId;
+  @NotEmpty
+  private String npmArtifactId;
+  @NotEmpty
+  private String nodeJsBinariesRootUrl;
+  @NotEmpty
+  private String nodeJsBinariesUrl;
+  @NotEmpty
+  private String nodeJsBinariesUrlWindows;
+  @NotEmpty
+  private String nodeJsBinariesUrlWindowsX86Legacy;
+  @NotEmpty
+  private String nodeJsBinariesUrlWindowsX64Legacy;
+  @NotEmpty
+  private String npmBinariesUrl;
+  @NotEmpty
+  private String nodeJsChecksumUrl;
+  @NotEmpty
+  private String nodeJsSampleVersion;
+  @NotEmpty
+  private String npmSampleVersion;
+
+  @Valid
+  @NotNull
+  private HttpClientConfiguration httpClient = new HttpClientConfiguration();
+
+  @JsonProperty
+  public String getGroupId() {
+    return this.groupId;
+  }
+
+  @JsonProperty
+  public String getNodeJsArtifactId() {
+    return this.nodeJsArtifactId;
+  }
+
+  @JsonProperty
+  public String getNpmArtifactId() {
+    return this.npmArtifactId;
+  }
+
+  @JsonProperty
+  public String getNodeJsBinariesRootUrl() {
+    return this.nodeJsBinariesRootUrl;
+  }
+
+  @JsonProperty
+  public String getNodeJsBinariesUrl() {
+    return this.nodeJsBinariesUrl;
+  }
+
+  @JsonProperty
+  public String getNodeJsBinariesUrlWindows() {
+    return this.nodeJsBinariesUrlWindows;
+  }
+
+  @JsonProperty
+  public String getNodeJsBinariesUrlWindowsX86Legacy() {
+    return this.nodeJsBinariesUrlWindowsX86Legacy;
+  }
+
+  @JsonProperty
+  public String getNodeJsBinariesUrlWindowsX64Legacy() {
+    return this.nodeJsBinariesUrlWindowsX64Legacy;
+  }
+
+  @JsonProperty
+  public String getNpmBinariesUrl() {
+    return this.npmBinariesUrl;
+  }
+
+  @JsonProperty
+  public String getNodeJsChecksumUrl() {
+    return this.nodeJsChecksumUrl;
+  }
+
+  @JsonProperty
+  public String getNodeJsSampleVersion() {
+    return this.nodeJsSampleVersion;
+  }
+
+  @JsonProperty
+  public String getNpmSampleVersion() {
+    return this.npmSampleVersion;
+  }
+
+  @JsonProperty("httpClient")
+  public HttpClientConfiguration getHttpClient() {
+    return httpClient;
+  }
+
+}
---
+++ maven-aem-binaries-proxy/src/main/java/io/wcm/devops/maven/aembinariesproxy/resource/IndexPageBuilder.java
@@ -0,0 +1,88 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2017 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.devops.maven.aembinariesproxy.resource;
+
+import org.apache.commons.lang3.StringUtils;
+
+import io.wcm.devops.maven.aembinariesproxy.MavenProxyConfiguration;
+
+/**
+ * Builds HTML index page
+ */
+public final class IndexPageBuilder {
+
+  private static final String[] EXAMPLE_URLS = new String[] {
+      "${groupIdPath}/${nodeJsArtifactId}/${nodeJsSampleVersion}/${nodeJsArtifactId}-${nodeJsSampleVersion}.pom",
+      "${groupIdPath}/${nodeJsArtifactId}/${nodeJsSampleVersion}/${nodeJsArtifactId}-${nodeJsSampleVersion}-windows-x86.exe",
+      "${groupIdPath}/${nodeJsArtifactId}/${nodeJsSampleVersion}/${nodeJsArtifactId}-${nodeJsSampleVersion}-windows-x64.exe",
+      "${groupIdPath}/${nodeJsArtifactId}/${nodeJsSampleVersion}/${nodeJsArtifactId}-${nodeJsSampleVersion}-linux-x86.tar.gz",
+      "${groupIdPath}/${nodeJsArtifactId}/${nodeJsSampleVersion}/${nodeJsArtifactId}-${nodeJsSampleVersion}-linux-x64.tar.gz",
+      "${groupIdPath}/${nodeJsArtifactId}/${nodeJsSampleVersion}/${nodeJsArtifactId}-${nodeJsSampleVersion}-darwin-x64.tar.gz",
+      "${groupIdPath}/${npmArtifactId}/${npmSampleVersion}/${npmArtifactId}-${npmSampleVersion}.pom",
+      "${groupIdPath}/${npmArtifactId}/${npmSampleVersion}/${npmArtifactId}-${npmSampleVersion}.tgz"
+  };
+
+  private IndexPageBuilder() {
+    // static methods only
+  }
+
+  /**
+   * Build HTML index page
+   */
+  public static String build(MavenProxyConfiguration config) {
+    StringBuilder exampleUrlsMarkup = new StringBuilder();
+    for (String exampleUrl : EXAMPLE_URLS) {
+      String url = exampleUrl;
+      url = StringUtils.replace(url, "${groupIdPath}", StringUtils.replace(config.getGroupId(), ".", "/"));
+      url = StringUtils.replace(url, "${nodeJsArtifactId}", config.getNodeJsArtifactId());
+      url = StringUtils.replace(url, "${npmArtifactId}", config.getNpmArtifactId());
+      url = StringUtils.replace(url, "${nodeJsSampleVersion}", config.getNodeJsSampleVersion());
+      url = StringUtils.replace(url, "${npmSampleVersion}", config.getNpmSampleVersion());
+      exampleUrlsMarkup.append("<li><a href=\"").append(url).append("\">").append(url).append("</a></li>");
+    }
+
+    String serviceVersion = IndexPageBuilder.class.getPackage().getImplementationVersion();
+
+    return "<!DOCTYPE html>\n<html>"
+    + "<head>"
+    + "<title>Maven NodeJS Proxy</title>"
+    + "<style>body { font-family: sans-serif; }</style>"
+    + "</head>"
+    + "<body>"
+    + "<h1>Maven NodeJS Proxy</h1>"
+    + "<p>This is a Maven Artifact Proxy for NodeJS binaries located at: "
+    + "<a href=\"" + config.getNodeJsBinariesRootUrl() + "\">" + config.getNodeJsBinariesRootUrl() + "</a></p>"
+    + "<p>Every call to this Maven repository is routed directly to the NodeJS distribution server.</p>"
+    + "<p><strong>Please never use this Maven repository directly in your maven builds, use it only via a Repository Manager "
+    + "which caches the resolved artifacts.</strong></p>"
+    + "<p>If you want to setup your own proxy get the source code: "
+    + "<a href=\"https://github.com/wcm-io-devops/maven-nodejs-proxy\">https://github.com/wcm-io-devops/maven-nodejs-proxy</a></p>"
+    + "<hr/>"
+    + "<p>Example artifacts:</p>"
+    + "<ul>"
+    + exampleUrlsMarkup
+    + "</ul>"
+    + "<p>For all files SHA1 checksums are supported (.sha1 suffix). MD5 checksums are not supported.</p>"
+    + (serviceVersion != null ? "<hr/><p>Version " + IndexPageBuilder.class.getPackage().getImplementationVersion() + "</p>" : "")
+    + "</body>"
+    + "</html>";
+  }
+
+}
